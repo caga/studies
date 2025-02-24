3eda971abae4dc5ea0976b62ca505dd52fd1d553
caga <cagataycakir@gmail.com>
2025-02-24 13:37:00 +0300

    lasts

---
+++ C_programming/den.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main () {
+// Only one condition fires!!!
+	if(0==1){
+		printf("The universe is broken!\n");
+	}else if(1==1){
+		printf("1==1\n");
+	}else if(1==1){
+		printf("hope");
+	}
+	else{
+		printf("All is well in the math world\n");
+
+	}
+
+	return 0;
+
+}
--- C_programming/typesize.c
+++ C_programming/typesize.c
@@ -2,7 +2,13 @@
 
 int main(){
 
+	printf("sizeof: char = %d\n", sizeof(char));
+	printf("sizeof: short = %d\n", sizeof(short));
 	printf("sizeof: int = %d\n", sizeof(int));
+	printf("sizeof: uint = %d\n", sizeof(unsigned int));
+	printf("sizeof: float = %d\n", sizeof(float));
+	printf("sizeof: long = %d\n", sizeof(long));
+	printf("sizeof: long long = %d\n", sizeof(long long));
+	printf("sizeof: double = %d\n", sizeof(double));
 	return 0;
-	hof
 }
